2adf3be2ad3a7f52620edca2f67223faf7513340
drxwat <drxwat@gmail.com>
2022-07-28 11:38:41 +0200

    fix points formatter

--- game/hud/HUD.gd
+++ game/hud/HUD.gd
@@ -3,4 +3,4 @@ extends MarginContainer
 onready var points_label := $VBoxContainer/HBoxContainer/PointsLabel
 
 func set_points(points: int) -> void:
-	points_label.text = "%d" % points
+	points_label.text = "%06d" % points
